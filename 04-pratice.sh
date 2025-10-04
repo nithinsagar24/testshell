bdb9dc3ac0f4b51580625ba4bbea404bf1b7b009
nithinsagar24 <nithinsagar42@gmail.com>
2025-10-04 11:10:15 +0530

    pratice

--- 04-pratice.sh
+++ 04-pratice.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-line$=.sh
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run this script with root privelege"
     exit 1 # failure is other than 0
 fi
 
-while IFS= read -r line; do
-    echo "Line: $line"
-done < "$1"
